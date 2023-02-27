e64dc36469441464c36ab1a6e0d43f351394427b
mac <mac@macs-MacBook-Pro.local>
2023-02-27 08:43:32 +0545

    [NEW] form initialize and discord webhook

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Whitelist from "./Pages/Whitelist";
+
+function App() {
+  return (
+    <div className="App">
+      <Whitelist />
+    </div>
+  );
+}
+
+export default App;
